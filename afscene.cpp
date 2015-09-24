dd92aa3bd395f1448f6420241ac6f81e71d42582
Karthik Periagaram <dekonvoluted@gmail.com>
2015-09-23 19:13:28 -0700

    Show a simple rectangle on the scene
    A green rectangle on a black background is now shown.

--- afscene.cpp
+++ afscene.cpp
@@ -1,5 +1,11 @@
 #include "afscene.h"
 
 AntFarmScene::AntFarmScene( QObject* parent ) : QGraphicsScene( parent )
-{}
+{
+    this->setSceneRect( 0, 0, 600, 600 );
+
+    setBackgroundBrush( Qt::black );
+
+    this->addRect( QRectF( 300, 300, 100, 100 ), QPen( Qt::white ), QBrush( Qt::green ) );
+}
 
