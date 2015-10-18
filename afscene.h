9072438f32ca09905075576abe11130a93925bd1
Karthik Periagaram <dekonvoluted@gmail.com>
2015-10-18 16:38:50 -0700

    Implement background and foreground
    The background consists of "white" squares that have either not been
    visited by the ant, or have been flipped back to the default color. The
    foreground consists of "black" squares that have been visited by the ant
    and flipped.
    
    A list keeps a record of the flipped points that are supposed to be
    black. Points are added and removed from this list.
    
    The rendered grid is infinite.

--- afscene.h
+++ afscene.h
@@ -3,10 +3,23 @@
 
 #include <QGraphicsScene>
 
+class QPainter;
+class QPixmap;
+
 class AntFarmScene : public QGraphicsScene
 {
     public:
     AntFarmScene( QObject* = 0 );
+
+    protected:
+    void drawBackground( QPainter*, const QRectF& );
+    void drawForeground( QPainter*, const QRectF& );
+
+    private:
+    QList<QPoint> flippedSquares;
+
+    QPixmap whiteSquare;
+    QPixmap blackSquare;
 };
 
 #endif
